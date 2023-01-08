2b5b41e792d75e8ab430219742f22d0edaae0f55
Choi ByungJae <wlvvwhs@naver.com>
2023-01-08 21:23:09 +0900

    20230108-멀티스레드 요청 시 문제

--- src/test/java/study/stock/service/StockServiceTest.java
+++ src/test/java/study/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import study.stock.domain.Stock;
 import study.stock.repository.StockRepository;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -38,6 +42,32 @@ class StockServiceTest {
 
         Stock stock = stockRepository.findByProductId(1L).orElseThrow();
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        //비동기로 실행되는 작업을 단순화해서 사용하게 도와주는 자바의 api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        //카운트다운래치는 다른 스래드에서 수행중인 작업을 완료될때까지 대기하도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L,1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        //100 - 100 = 0
+        assertEquals(0L,stock.getQuantity());
     }
 
 }
