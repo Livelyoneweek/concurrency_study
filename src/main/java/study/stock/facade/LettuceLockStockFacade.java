549de1adb3991ec57b5209d5297871699de22f32
Livelyoneweek <wlvvwhs@naver.com>
2023-01-11 23:16:05 +0900

    2300111-redis lock(lettuce, redisson)

---
+++ src/main/java/study/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,32 @@
+package study.stock.facade;
+
+import org.springframework.stereotype.Component;
+import study.stock.repository.RedisLockRepository;
+import study.stock.service.StockService;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private RedisLockRepository redisLockRepository;
+
+    private StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long key, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(key)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(key, quantity);
+        } finally {
+            redisLockRepository.unlock(key);
+        }
+    }
+
+
+}
---
+++ src/test/java/study/stock/facade/RedissonLockStockFacadeTest.java
@@ -0,0 +1,65 @@
+package study.stock.facade;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.stock.domain.Stock;
+import study.stock.repository.StockRepository;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RedissonLockStockFacadeTest {
+
+    @Autowired
+    private RedissonLockStockFacade redissonLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void delete() {
+        stockRepository.deleteAll();
+    }
+
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        //비동기로 실행되는 작업을 단순화해서 사용하게 도와주는 자바의 api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        //카운트다운래치는 다른 스래드에서 수행중인 작업을 완료될때까지 대기하도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    redissonLockStockFacade.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        //100 - 100 = 0
+        assertEquals(0L,stock.getQuantity());
+    }
+
+}
